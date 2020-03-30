3a12f67d77965aeaa380bc26c74555e4f65e9cac
GOSSENT <kevin.gossent@capgemini.com>
2020-03-30 18:16:06 +0200

    check inputs for join
    pass tests

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -1,11 +1,16 @@
 import {Command, flags} from '@oclif/command'
+import * as fs from 'fs'
 
 export default class Join extends Command {
   static description = 'describe the command here'
 
   static flags = {
     help: flags.help({char: 'h'}),
-    meta: flags.string({char: 'm', description: 'path(s) to file(s) to join', multiple: true}),
+    meta: flags.string({
+      char: 'm',
+      description: 'path(s) to file(s) to join',
+      multiple: true,
+    }),
   }
 
   async run() {
@@ -14,6 +19,18 @@ export default class Join extends Command {
     // const name = flags.name || 'world'
     // this.log(`hello ${name} from C:\\GitRepos\\sfdc_md_merge_driver\\src\\commands\\join.ts`)
 
+    if (flags.meta === undefined) {
+      console.log('list of permissions to merge is empty')
+      // eslint-disable-next-line no-throw-literal
+      throw 'list of permissions to merge is empty'
+    }
+    flags.meta.forEach(permission => {
+      if (!fs.existsSync(permission)) {
+        console.error(`${permission} is not accessible`)
+        // eslint-disable-next-line no-throw-literal
+        throw `${permission} is not accessible`
+      }
+    })
     console.log(flags.meta)
   }
 }
--- test/commands/join.test.ts
+++ test/commands/join.test.ts
@@ -2,16 +2,9 @@ import {expect, test} from '@oclif/test'
 
 describe('join', () => {
   test
-  .stdout()
-  .command(['join'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['join', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['join'])
+    .it('runs hello', ctx => {
+      expect(ctx.stdout).to.contain('')
+    })
 })
