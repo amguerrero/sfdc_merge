94d10ea953ce4036dc256fa2e4268bf2f1c47c8a
GOSSENT <kevin.gossent@capgemini.com>
2020-04-03 18:39:16 +0200

    Finish
    Adding file list read async
    Adding join based on reducer (treating unique and exclusive differently)
    Adding writing file (sync, to see if faster with async)
    Adding verbose mode

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -1,6 +1,11 @@
 import {Command, flags} from '@oclif/command'
 import * as fs from 'fs'
-import {getMetadataType, getMetaConfigJSON} from '../utils/file-helper'
+import {
+  getMetadataType,
+  getMetaConfigJSON,
+  getFiles,
+  writeOutput,
+} from '../utils/file-helper'
 
 export default class Join extends Command {
   static description = 'describe the command here'
@@ -12,11 +17,22 @@ export default class Join extends Command {
       description: 'path(s) to file(s) to join',
       multiple: true,
     }),
+    output: flags.string({
+      char: 'o',
+      description: 'path to write output',
+    }),
+    verbose: flags.boolean({
+      char: 'v',
+      description: 'verbose mode',
+    }),
   }
 
   async run() {
+    const tStart = Date.now()
+    const verboseTab = []
     const {flags} = this.parse(Join)
 
+    let stepStart = Date.now()
     if (flags.meta === undefined) {
       console.log('list of permissions to merge is empty')
       // eslint-disable-next-line no-throw-literal
@@ -29,32 +45,119 @@ export default class Join extends Command {
         throw `${permission} is not accessible`
       }
     })
-    console.log(flags.meta)
-    // let meta
-    // await getMetadataType(flags.meta).then(data => {
-    //   meta = data
-    // })
-    // console.log('meta: ' + meta)
+    let stepEnd = Date.now()
+    verboseTab.push({'input check time:': stepEnd - stepStart})
+
+    stepStart = Date.now()
     let meta
     await getMetadataType(flags.meta)
       .then(result => {
         meta = result
-        console.log('result meta:', meta)
+        verboseTab.push({'meta to join:': meta})
       })
       .catch(error => {
         console.error(error)
         throw error
       })
+    stepEnd = Date.now()
+    verboseTab.push({'get metadaType time:': stepEnd - stepStart})
+
+    stepStart = Date.now()
+    let configJson
     await getMetaConfigJSON(meta)
       .then(result => {
-        console.log(JSON.stringify(result))
+        configJson = result
       })
       .catch(error => {
         console.error(error)
         throw error
       })
-    // await getMetaConfigJSON(meta).then(result => {
-    //   console.log(result)
-    // })
+    stepEnd = Date.now()
+    verboseTab.push({'get config time: ': stepEnd - stepStart})
+
+    stepStart = Date.now()
+    let fileJSON
+    await getFiles(flags.meta, meta).then(result => {
+      fileJSON = result
+    })
+    stepEnd = Date.now()
+    verboseTab.push({'get files time:': stepEnd - stepStart})
+
+    stepStart = Date.now()
+    function joinUniques(ours, theirs, attributs) {
+      ours
+        .filter(
+          x =>
+            !theirs.find(
+              y =>
+                attributs.reduce(
+                  (acc, attribut) => `${acc}${JSON.stringify(x[attribut])}#`,
+                  '',
+                ) ===
+                attributs.reduce(
+                  (acc, attribut) => `${acc}${JSON.stringify(y[attribut])}#`,
+                  '',
+                ),
+            ),
+        )
+        .concat(theirs)
+    }
+    function joinExclusives(ours, theirs, attributs) {
+      ours
+        .filter(
+          x =>
+            !theirs.find(
+              y =>
+                Array.of(attributs.find(att => y[att])).reduce(
+                  (acc, attribut) => `${acc}${JSON.stringify(x[attribut])}#`,
+                  '',
+                ) ===
+                Array.of(attributs.find(att => y[att])).reduce(
+                  (acc, attribut) => `${acc}${JSON.stringify(y[attribut])}#`,
+                  '',
+                ),
+            ),
+        )
+        .concat(theirs)
+    }
+    const reducer = function(acc, curr) {
+      // first loop we will use the current Permission => no merge required :D
+      if (Object.entries(acc).length === 0 && acc.constructor === Object) {
+        return curr
+      }
+      Object.keys(curr).forEach(p => {
+        if (configJson.uniqueKeys && configJson.uniqueKeys[p]) {
+          joinUniques(acc[p] || [], curr[p] || [], configJson.uniqueKeys[p])
+        } else if (
+          configJson.exclusiveUniqueKeys &&
+          configJson.exclusiveUniqueKeys[p]
+        ) {
+          joinExclusives(
+            acc[p] || [],
+            curr[p] || [],
+            configJson.exclusiveUniqueKeys[p],
+          )
+        } else {
+          verboseTab.push({'unlisted property:': p})
+        }
+      })
+      return acc
+    }
+    const merged = fileJSON.reduce(reducer, {})
+    stepEnd = Date.now()
+    verboseTab.push({'join time:': stepEnd - stepStart})
+
+    stepStart = Date.now()
+    await writeOutput(meta, flags.output, merged)
+    stepEnd = Date.now()
+    verboseTab.push({'writing time:': stepEnd - stepStart})
+
+    const tEnd = Date.now()
+    if (flags.verbose) {
+      for (const elem of verboseTab) {
+        console.log(Object.keys(elem)[0], Object.values(elem)[0])
+      }
+      console.log('teatment time:', tEnd - tStart)
+    }
   }
 }
--- src/utils/file-helper.ts
+++ src/utils/file-helper.ts
@@ -1,10 +1,17 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import * as es from 'event-stream'
+import * as xml2js from 'xml2js'
 
 const regProfile = /.*<Profile xmlns/
 const regPSet = /.*<PermissionSet xmlns/
 const regLabel = /.*<CustomLabels xmlns/
+
+const builder = new xml2js.Builder({
+  xmldec: {version: '1.0', encoding: 'UTF-8'},
+  renderOpts: {pretty: true, indent: '    ', newline: '\n'},
+  xmlns: true,
+})
 
 async function getMetafromFile(file) {
   return new Promise(resolve => {
@@ -63,12 +70,12 @@ export async function getMetaConfigJSON(meta) {
       )
       .on('end', () => {
         const jsonO = JSON.parse(output)
-        const result = {}
+        const result = {uniqueKeys: {}, exclusiveUniqueKeys: {}}
         for (const x of Object.keys(jsonO)) {
-          result[x] =
-            jsonO[x].uniqueKeys === undefined
-              ? jsonO[x].exclusiveUniqueKeys
-              : jsonO[x].uniqueKeys
+          // result[x] =
+          jsonO[x].uniqueKeys === undefined
+            ? (result.exclusiveUniqueKeys[x] = jsonO[x].exclusiveUniqueKeys)
+            : (result.uniqueKeys[x] = jsonO[x].uniqueKeys)
         }
         resolve(result)
       })
@@ -96,4 +103,114 @@ export async function getMetadataType(files: string[]) {
       throw error
     })
   return output
+}
+
+/* function getNodeBaseXML(meta) {
+  switch (meta) {
+    default:
+      return '<?xml version="1.0" encoding="UTF-8"?><'.concat(meta).concat(' xmlns="http://soap.sforce.com/2006/04/metadata"></').concat(meta).concat('>')
+  }
+} */
+
+function getNodeBaseJSON(meta) {
+  const result = {}
+  switch (meta) {
+    default:
+      result[meta] = {$: {xmlns: 'http://soap.sforce.com/2006/04/metadata'}}
+      return result
+    // return JSON.parse('{"' + meta + '":{ "$": { "xmlns": "http://soap.sforce.com/2006/04/metadata" }}}')
+  }
+}
+
+/* async function getNodes(file, meta) {
+  const tmpFile = fs.existsSync(file) ? fs.readFileSync(file, 'utf8') : ''
+  if (tmpFile.length === 0) {
+    return {}
+    // tmpFile = getNodeBaseXML(meta)
+  }
+  let result
+  new xml2js.Parser().parseString(tmpFile, (e, r) => {
+    result = r
+  })
+  return result[meta]
+}
+
+async function getNodes2(file, meta) {
+  return new Promise(resolve => {
+    let output = ''
+    fs.createReadStream(file)
+      .pipe(
+        es.mapSync(function (data) {
+          // console.log(data)
+          output = output.concat(data)
+        }),
+      )
+      .on('end', () => {
+        if (output.length > 0) {
+          xml2js.parseString(output, (e, r) => {
+            resolve(r[meta])
+          })
+        } else {
+          resolve({})
+        }
+        // resolve(result)
+      })
+  }).then(result => {
+    // console.log(JSON.stringify(result["applicationVisibilities"]))
+    result["_fileName"] = file
+    return result
+  })
+} */
+
+async function getNodes3(file, meta) {
+  return new Promise(resolve => {
+    let output = ''
+    if (fs.statSync(file).size === 0) {
+      resolve({})
+    } else {
+      fs.createReadStream(file)
+        .pipe(
+          es.mapSync(function(data) {
+            // console.log(data)
+            output = output.concat(data)
+          }),
+        )
+        .on('end', () => {
+          // if (output.length > 0) {
+          xml2js.parseString(output, (e, r) => {
+            resolve(r[meta])
+          })
+          // } else {
+          //   resolve({})
+          // }
+          // resolve(result)
+        })
+    }
+  }).then(result => {
+    // console.log(JSON.stringify(result["applicationVisibilities"]))
+    // result["_fileName"] = file
+    return result
+  })
+}
+
+export async function getFiles(files: string[], meta) {
+  const tabPromise = []
+  let output
+  for (const key of files) {
+    tabPromise.push(getNodes3(key, meta))
+  }
+  await Promise.all(tabPromise)
+    .then(data => {
+      output = data
+    })
+    .catch(error => {
+      throw error
+    })
+  return output
+}
+
+export async function writeOutput(meta, file, jsonOutput) {
+  const base = getNodeBaseJSON(meta)
+  Object.assign(base[meta], jsonOutput)
+  fs.writeFileSync(file, builder.buildObject(base))
 }
