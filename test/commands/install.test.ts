016bf82e7cdd413159b7e6904f23693b7238abb4
GOSSENT <kevin.gossent@capgemini.com>
2020-04-07 13:11:57 +0200

    Make tests work even on windows
    make timeout of install test higher because it is often close to the standard one

--- test/commands/install.test.ts
+++ test/commands/install.test.ts
@@ -2,9 +2,10 @@ import {expect, test} from '@oclif/test'
 
 describe('install', () => {
   test
+    .timeout(10000)
     .stdout()
     .command(['install'])
-    .it('runs install', ctx => {
+    .it('runs install', (ctx) => {
       expect(ctx.stdout).to.contain('')
     })
 })
