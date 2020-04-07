e1bb836d1b179e4dcfead6ea14c72d9611f2cbba
GOSSENT <kevin.gossent@capgemini.com>
2020-04-07 18:12:19 +0200

    better dealing with bad file input and putting it in new helper

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -1,10 +1,10 @@
 import {Command, flags} from '@oclif/command'
-import * as fs from 'fs'
 import {
   getMetadataType,
   getMetaConfigJSON,
   getFiles,
   writeOutput,
+  allFilesExist,
 } from '../utils/file-helper'
 import {joinUniques, joinExclusives} from '../utils/merge-helper'
 import {addVerboseInfo, printVerboseInfo} from '../utils/verbose-helper'
@@ -36,17 +36,38 @@ export default class Join extends Command {
 
     let stepStart = Date.now()
     if (flags.meta === undefined) {
-      console.log('list of permissions to merge is empty')
+      console.error('list of permissions to merge is empty')
+      if (flags.verbose) printVerboseInfo(verboseTab, tStart)
+      return ''
       // eslint-disable-next-line no-throw-literal
-      throw 'list of permissions to merge is empty'
+      // throw 'list of permissions to merge is empty'
     }
-    flags.meta.forEach((permission) => {
-      if (!fs.existsSync(permission)) {
-        console.error(`${permission} is not accessible`)
-        // eslint-disable-next-line no-throw-literal
-        throw `${permission} is not accessible`
-      }
-    })
+    if (!(await allFilesExist(flags.meta))) {
+      console.error('at least a metadataFile is not accessible')
+      if (flags.verbose) printVerboseInfo(verboseTab, tStart)
+      return ''
+    }
+    // flags.meta.forEach((permission) => {
+    //   console.log('test file', permission)
+    //   // try {
+    //   //   fs.access(permission, error => {
+    //   //     if (error) {
+    //   //       console.log(`${permission} is not accessible1`)
+    //   //       return ''
+    //   //     }
+    //   //   })
+    //   // } catch (error) {
+    //   //   console.log(`${permission} is not accessible2`)
+    //   //   return ''
+    //   // }
+    //   if (!fs.existsSync(permission)) {
+    //     console.log(`${permission} is not accessible`)
+    //     return ''
+    //     // eslint-disable-next-line no-throw-literal
+    //     // throw `${permission} is not accessible`
+    //   }
+    // })
+    console.log('after files')
     if (flags.verbose)
       addVerboseInfo(verboseTab, stepStart, 'input check time:')
 
