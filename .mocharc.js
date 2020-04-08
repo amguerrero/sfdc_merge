263b0f0da8cabecc9fb3a575bafa84942d9f3879
GOSSENT <kevin.gossent@capgemini.com>
2020-04-08 08:47:59 +0200

    Migrate mocka option to non deprecated option file

---
+++ .mocharc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Here's a JavaScript-based config file.
+// If you need conditional logic, you might want to use this type of config.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+    require: 'ts-node/register',
+    'watch-extensions': 'ts',
+    recursive: true,
+    reporter: 'spec',
+    timeout: 10000,
+};
