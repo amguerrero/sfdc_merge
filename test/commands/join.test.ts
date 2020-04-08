3aa0a1a0e7688f0d1fd1a96d434c82cc0d224aea
GOSSENT <kevin.gossent@capgemini.com>
2020-04-08 09:09:59 +0200

    removing unneeded path and shell usage for windows compatibility usage

--- test/commands/join.test.ts
+++ test/commands/join.test.ts
@@ -1,12 +1,12 @@
 /* eslint-disable unicorn/no-process-exit, no-process-exit */
 import {expect, test} from '@oclif/test'
-import * as shell from 'shelljs'
-import * as path from 'path'
+// import * as shell from 'shelljs'
+// import * as path from 'path'
 
 describe('join', () => {
-  afterEach(function () {
-    shell.exec('git checkout -q -- test/files/*')
-  })
+  // afterEach(function () {
+  //   shell.exec('git checkout -q -- test/files/*')
+  // })
   const strJoin =
     '{"Profile":{"$":{"xmlns":"http://soap.sforce.com/2006/04/metadata"},"layoutAssignments":[{"layout":["Account-Account Layout"]},{"layout":["Account-Account Layout"],"recordType":["Account.Customer"]}],"userPermissions":[{"enabled":["false"],"name":["ViewSetup"]}]}}'
 
@@ -17,11 +17,11 @@ describe('join', () => {
     .command([
       'join',
       '-m',
-      path.join('.', 'test', 'files', 'ancestor.profile-meta.xml'),
+      './test/files/ancestor.profile-meta.xml',
       '-m',
-      path.join('.', 'test', 'files', 'ours.profile-meta.xml'),
+      './test/files/ours.profile-meta.xml',
       '-m',
-      path.join('.', 'test', 'files', 'theirs.profile-meta.xml'),
+      './test/files/theirs.profile-meta.xml',
       '-v',
     ])
     .it('runs join', (ctx) => {
@@ -46,11 +46,12 @@ describe('join', () => {
     .command([
       'join',
       '-m',
-      path
-        .join('.', 'test', 'files')
-        .toString()
-        .concat(path.sep)
-        .concat('non_existing.profile-meta.xml'),
+      './test/files/non_existing.profile-meta.xml',
+      // path
+      //   .join('.', 'test', 'files')
+      //   .toString()
+      //   .concat(path.sep)
+      //   .concat('non_existing.profile-meta.xml'),
     ])
     .it('runs join with inexisting file', (ctx) => {
       expect(process.exit()).to.equal('foobar')
