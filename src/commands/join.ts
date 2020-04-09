3fbd3d542cbe3c885998df76ea305a02c47bd20e
GOSSENT <kevin.gossent@capgemini.com>
2020-04-09 14:47:13 +0200

    Merge branch 'feature/test_perf_gros_fichier' into publish

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -2,11 +2,12 @@ import {Command, flags} from '@oclif/command'
 import {
   getMetadataType,
   getMetaConfigJSON,
-  getFiles,
+  // getFiles,
+  getKeyedFiles,
   writeOutput,
   allFilesExist,
 } from '../utils/file-helper'
-import {joinUniques, joinExclusives} from '../utils/merge-helper'
+// import {joinUniques, joinExclusives} from '../utils/merge-helper'
 import {addVerboseInfo, printVerboseInfo} from '../utils/verbose-helper'
 
 export default class Join extends Command {
@@ -74,47 +75,121 @@ export default class Join extends Command {
       })
     if (flags.verbose) addVerboseInfo(verboseTab, stepStart, 'get config time:')
 
-    stepStart = Date.now()
-    let fileJSON
-    await getFiles(flags.meta, meta).then((result) => {
-      fileJSON = result
-    })
-    if (flags.verbose) addVerboseInfo(verboseTab, stepStart, 'get files time:')
+    // stepStart = Date.now()
+    // let fileJSON
+    // await getFiles(flags.meta, meta).then((result) => {
+    //   fileJSON = result
+    // })
+    // // console.log('fileJSON', fileJSON)
+    // if (flags.verbose) addVerboseInfo(verboseTab, stepStart, 'get files time:')
+
+    // stepStart = Date.now()
+    // // console.log(
+    // //   'test key local'.padEnd(30),
+    // //   buildUniqueKey(
+    // //     fileJSON[0].layoutAssignments[0],
+    // //     'layoutAssignments',
+    // //     configJson,
+    // //   ),
+    // // )
+    // const reducer = function (acc, curr) {
+    //   // first loop we will use the current Permission => no merge required :D
+    //   if (Object.entries(acc).length === 0 && acc.constructor === Object) {
+    //     return curr
+    //   }
+    //   Object.keys(curr).forEach((p) => {
+    //     // if (configJson.uniqueKeys && configJson.uniqueKeys[p]) {
+    //     //   acc[p] = joinUniques(
+    //     //     acc[p] || [],
+    //     //     curr[p] || [],
+    //     //     configJson.uniqueKeys[p],
+    //     //   )
+    //     // } else if (
+    //     //   configJson.exclusiveUniqueKeys &&
+    //     //   configJson.exclusiveUniqueKeys[p]
+    //     // ) {
+    //     //   acc[p] = joinExclusives(
+    //     //     acc[p] || [],
+    //     //     curr[p] || [],
+    //     //     configJson.exclusiveUniqueKeys[p],
+    //     //   )
+    //     // } else {
+    //     //   verboseTab.push({'unlisted property:': p})
+    //     // }
+    //     if (configJson[p]) {
+    //       if (configJson[p].uniqueKeys) {
+    //         acc[p] = joinUniques(
+    //           acc[p] || [],
+    //           curr[p] || [],
+    //           configJson[p].uniqueKeys,
+    //         )
+    //       } else {
+    //         acc[p] = joinExclusives(
+    //           acc[p] || [],
+    //           curr[p] || [],
+    //           configJson[p].exclusiveUniqueKeys,
+    //         )
+    //       }
+    //     } else {
+    //       verboseTab.push({'unlisted property:': p})
+    //     }
+    //   })
+    //   return acc
+    // }
+    // const merged = fileJSON.reduce(reducer, {})
+    // if (flags.verbose) addVerboseInfo(verboseTab, stepStart, 'join time:')
 
     stepStart = Date.now()
-    const reducer = function (acc, curr) {
+    let fileKeyedJSON
+    await getKeyedFiles(flags.meta, meta, configJson).then((result) => {
+      fileKeyedJSON = result
+    })
+    // console.log('fileKeyedJSON', fileKeyedJSON)
+    if (flags.verbose)
+      addVerboseInfo(verboseTab, stepStart, 'get keyed files time:')
+
+    stepStart = Date.now()
+    const reducerKeyed = function (acc, curr) {
       // first loop we will use the current Permission => no merge required :D
       if (Object.entries(acc).length === 0 && acc.constructor === Object) {
         return curr
       }
       Object.keys(curr).forEach((p) => {
-        if (configJson.uniqueKeys && configJson.uniqueKeys[p]) {
-          acc[p] = joinUniques(
-            acc[p] || [],
-            curr[p] || [],
-            configJson.uniqueKeys[p],
-          )
-        } else if (
-          configJson.exclusiveUniqueKeys &&
-          configJson.exclusiveUniqueKeys[p]
-        ) {
-          acc[p] = joinExclusives(
-            acc[p] || [],
-            curr[p] || [],
-            configJson.exclusiveUniqueKeys[p],
-          )
-        } else {
-          verboseTab.push({'unlisted property:': p})
-        }
+        acc[p] = curr[p]
       })
       return acc
     }
-    const merged = fileJSON.reduce(reducer, {})
-    if (flags.verbose) addVerboseInfo(verboseTab, stepStart, 'join time:')
+    const mergedKeyed = fileKeyedJSON.reduce(reducerKeyed, {})
+    // console.log('mergedKeyed', mergedKeyed)
+    if (flags.verbose) addVerboseInfo(verboseTab, stepStart, 'join keyed time:')
 
     stepStart = Date.now()
-    await writeOutput(meta, flags.output, merged)
-    if (flags.verbose) addVerboseInfo(verboseTab, stepStart, 'writing time:')
+    const unKeyed = {}
+    Object.keys(mergedKeyed)
+      .sort()
+      .forEach(function (key) {
+        // eslint-disable-next-line no-negated-condition
+        if (mergedKeyed[key].nodeType !== '$') {
+          if (!Array.isArray(unKeyed[mergedKeyed[key].nodeType])) {
+            unKeyed[mergedKeyed[key].nodeType] = []
+          }
+          unKeyed[mergedKeyed[key].nodeType].push(mergedKeyed[key].node)
+        } else {
+          unKeyed[mergedKeyed[key].nodeType] = mergedKeyed[key].node
+        }
+      })
+    // console.log('unKeyed', unKeyed)
+    if (flags.verbose)
+      addVerboseInfo(verboseTab, stepStart, 'transform keyed to unkeyed:')
+
+    // stepStart = Date.now()
+    // await writeOutput(meta, flags.output, merged)
+    // if (flags.verbose) addVerboseInfo(verboseTab, stepStart, 'writing time:')
+
+    stepStart = Date.now()
+    await writeOutput(meta, flags.output, unKeyed)
+    if (flags.verbose)
+      addVerboseInfo(verboseTab, stepStart, 'writing keyed time:')
 
     if (flags.verbose) printVerboseInfo(verboseTab, tStart)
     console.error('sfdx-md-merge-driver:', 'successfully joined.')
