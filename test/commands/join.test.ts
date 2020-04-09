b794817cec3c8264ab7347756784765657144835
GOSSENT <kevin.gossent@capgemini.com>
2020-04-09 08:11:19 +0200

    remove comments in tests

--- test/commands/join.test.ts
+++ test/commands/join.test.ts
@@ -1,12 +1,7 @@
 /* eslint-disable unicorn/no-process-exit, no-process-exit */
 import {expect, test} from '@oclif/test'
-// import * as shell from 'shelljs'
-// import * as path from 'path'
 
 describe('join', () => {
-  // afterEach(function () {
-  //   shell.exec('git checkout -q -- test/files/*')
-  // })
   const strJoin =
     '{"Profile":{"$":{"xmlns":"http://soap.sforce.com/2006/04/metadata"},"layoutAssignments":[{"layout":["Account-Account Layout"]},{"layout":["Account-Account Layout"],"recordType":["Account.Customer"]}],"userPermissions":[{"enabled":["false"],"name":["ViewSetup"]}]}}'
 
@@ -43,16 +38,7 @@ describe('join', () => {
   test
     .stub(process, 'exit', () => 'foobar')
     .stderr()
-    .command([
-      'join',
-      '-m',
-      './test/files/non_existing.profile-meta.xml',
-      // path
-      //   .join('.', 'test', 'files')
-      //   .toString()
-      //   .concat(path.sep)
-      //   .concat('non_existing.profile-meta.xml'),
-    ])
+    .command(['join', '-m', './test/files/non_existing.profile-meta.xml'])
     .it('runs join with inexisting file', (ctx) => {
       expect(process.exit()).to.equal('foobar')
       expect(ctx.stderr).to.contain('at least a metadataFile is not accessible')
