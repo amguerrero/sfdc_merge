2675cb63c9dca753769fa3f5da5d8f38714729c7
GOSSENT <kevin.gossent@capgemini.com>
2020-04-09 15:09:25 +0200

    Remove commented code and orphan functions

--- src/utils/file-helper.ts
+++ src/utils/file-helper.ts
@@ -69,29 +69,6 @@ function getConfigPath(meta) {
 
 export async function getMetaConfigJSON(meta) {
   return new Promise((resolve) => {
-    // let output = ''
-    // fs.createReadStream(getConfigPath(meta), {flags: 'r', encoding: 'utf8'})
-    //   // .pipe(
-    //   //   es.mapSync(function (data) {
-    //   //     // console.log(data)
-    //   //     output = output.concat(data)
-    //   //   }),
-    //   // )
-    //   .on('data', (chunk) => {
-    //     output = output.concat(chunk.toString())
-    //   })
-    //   .on('end', () => {
-    //     const jsonO = JSON.parse(output)
-    //     // const result = {uniqueKeys: {}, exclusiveUniqueKeys: {}}
-    //     // for (const x of Object.keys(jsonO)) {
-    //     //   // result[x] =
-    //     //   jsonO[x].uniqueKeys === undefined
-    //     //     ? (result.exclusiveUniqueKeys[x] = jsonO[x].exclusiveUniqueKeys)
-    //     //     : (result.uniqueKeys[x] = jsonO[x].uniqueKeys)
-    //     // }
-    //     // resolve(result)
-    //     resolve(jsonO)
-    //   })
     fs.readFile(
       getConfigPath(meta),
       {flag: 'r', encoding: 'utf8'},
@@ -126,13 +103,6 @@ export async function getMetadataType(files: string[]) {
   return output
 }
 
-/* function getNodeBaseXML(meta) {
-  switch (meta) {
-    default:
-      return '<?xml version="1.0" encoding="UTF-8"?><'.concat(meta).concat(' xmlns="http://soap.sforce.com/2006/04/metadata"></').concat(meta).concat('>')
-  }
-} */
-
 function getNodeBaseJSON(meta) {
   const result = {}
   switch (meta) {
@@ -141,69 +111,6 @@ function getNodeBaseJSON(meta) {
       return result
   }
 }
-
-/* async function getNodes(file, meta) {
-  const tmpFile = fs.existsSync(file) ? fs.readFileSync(file, 'utf8') : ''
-  if (tmpFile.length === 0) {
-    return {}
-    // tmpFile = getNodeBaseXML(meta)
-  }
-  let result
-  new xml2js.Parser().parseString(tmpFile, (e, r) => {
-    result = r
-  })
-  return result[meta]
-}
-
-async function getNodes2(file, meta) {
-  return new Promise(resolve => {
-    let output = ''
-    fs.createReadStream(file)
-      .pipe(
-        es.mapSync(function (data) {
-          // console.log(data)
-          output = output.concat(data)
-        }),
-      )
-      .on('end', () => {
-        if (output.length > 0) {
-          xml2js.parseString(output, (e, r) => {
-            resolve(r[meta])
-          })
-        } else {
-          resolve({})
-        }
-        // resolve(result)
-      })
-  }).then(result => {
-    // console.log(JSON.stringify(result["applicationVisibilities"]))
-    result["_fileName"] = file
-    return result
-  })
-} */
-
-// async function getNodes(file) {
-//   return new Promise((resolve) => {
-//     let output = ''
-//     if (fs.statSync(file).size === 0) {
-//       resolve({})
-//     } else {
-//       fs.createReadStream(file)
-//         .pipe(
-//           es.mapSync(function (data) {
-//             output = output.concat(data)
-//           }),
-//         )
-//         .on('end', () => {
-//           xml2js.parseString(output, (e, r) => {
-//             resolve(r)
-//           })
-//         })
-//     }
-//   }).then((result) => {
-//     return result
-//   })
-// }
 
 async function getNodesFromXmlFile(file) {
   return new Promise((resolve) => {
@@ -251,22 +158,6 @@ async function getKeyedNodesOfMeta(file, meta, configJson) {
     }
     return {}
   })
-}
-
-export async function getFiles(files: string[], meta) {
-  const tabPromise = []
-  let output
-  for (const key of files) {
-    tabPromise.push(getNodesOfMeta(key, meta))
-  }
-  await Promise.all(tabPromise)
-    .then((data) => {
-      output = data
-    })
-    .catch((error) => {
-      throw error
-    })
-  return output
 }
 
 export async function getKeyedFiles(files: string[], meta, configJson) {
--- src/utils/merge-helper.ts
+++ src/utils/merge-helper.ts
@@ -1,58 +1,5 @@
-export function joinUniques(ours, theirs, attributs) {
-  return ours
-    .filter(
-      (x) =>
-        !theirs.find(
-          (y) =>
-            attributs.reduce(
-              (acc, attribut) => `${acc}${JSON.stringify(x[attribut])}`,
-              '',
-            ) ===
-            attributs.reduce(
-              (acc, attribut) => `${acc}${JSON.stringify(y[attribut])}`,
-              '',
-            ),
-        ),
-    )
-    .concat(theirs)
-}
-export function joinExclusives(ours, theirs, attributs) {
-  return ours
-    .filter(
-      (x) =>
-        !theirs.find(
-          (y) =>
-            Array.of(attributs.find((att) => y[att])).reduce(
-              (acc, attribut) => `${acc}${JSON.stringify(x[attribut])}`,
-              '',
-            ) ===
-            Array.of(attributs.find((att) => y[att])).reduce(
-              (acc, attribut) => `${acc}${JSON.stringify(y[attribut])}`,
-              '',
-            ),
-        ),
-    )
-    .concat(theirs)
-}
-
 export function buildUniqueKey(node, type, configJson) {
   let uniqueKey = null
-  // if (configJson.uniqueKeys && configJson.uniqueKeys[type]) {
-  //   uniqueKey = configJson.uniqueKeys[type].reduce(
-  //     (acc, attribut) => acc.concat(node[attribut][0]).concat('#'),
-  //     type.concat('#'),
-  //   )
-  // } else if (
-  //   configJson.exclusiveUniqueKeys &&
-  //   configJson.exclusiveUniqueKeys[type]
-  // ) {
-  //   uniqueKey = Array.of(
-  //     configJson.exclusiveUniqueKeys[type].find((att) => node[att]),
-  //   ).reduce(
-  //     (acc, attribut) => acc.concat(node[attribut][0]).concat('#'),
-  //     type.concat('#'),
-  //   )
-  // }
   if (configJson[type]) {
     if (configJson[type].uniqueKeys) {
       uniqueKey = configJson[type].uniqueKeys.reduce(
