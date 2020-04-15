3f71104ee7a20cdae7c1ced88f2a1cf744b1dce2
GOSSENT <kevin.gossent@capgemini.com>
2020-04-15 12:18:03 +0200

    evol of building unique key: some unique keys are variable so we take all those existing

--- src/utils/merge-helper.ts
+++ src/utils/merge-helper.ts
@@ -3,7 +3,13 @@ export function buildUniqueKey(node, type, configJson) {
   if (configJson[type]) {
     if (configJson[type].uniqueKeys) {
       uniqueKey = configJson[type].uniqueKeys.reduce(
-        (acc, attribut) => acc.concat(node[attribut][0]).concat('#'),
+        // (acc, attribut) => acc.concat(node[attribut][0]).concat('#'),
+        (acc, attribut) => {
+          if (node[attribut]) {
+            return acc.concat(node[attribut][0]).concat('#')
+          }
+          return acc
+        },
         type.concat('#'),
       )
     } else {
