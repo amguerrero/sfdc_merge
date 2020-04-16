c1049a454ef513f8ea0f15aca1f4a9b75a555576
GOSSENT <kevin.gossent@capgemini.com>
2020-04-16 22:20:54 +0200

    using fs.promises
    group mots of submethods in one easier to read

--- src/utils/file-helper.ts
+++ src/utils/file-helper.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs'
+
 import * as path from 'path'
 import * as es from 'event-stream'
 // import * as xml2js from 'xml2js'
@@ -6,6 +7,7 @@ import * as xmljs from 'xml-js'
 import {buildUniqueKey} from '../utils/merge-helper'
 import {startTimer, endTimer} from '../utils/verbose-helper'
 
+const fsp = fs.promises
 const regGenericMatch = /(?<=<)(\w+)(?= +xmlns)/
 
 // const builder = new xml2js.Builder({
@@ -116,41 +118,6 @@ function getNodeBaseJSON(meta) {
   }
 }
 
-async function getNodesFromXmlFile(file, index, verbose: boolean) {
-  return new Promise((resolve) => {
-    startTimer('reading file: ' + file + ' index: ' + index, verbose)
-    fs.readFile(file, {flag: 'r', encoding: 'utf8'}, (err, data) => {
-      endTimer('reading file: ' + file + ' index: ' + index, verbose)
-      if (err) throw err
-      startTimer('xml parsing file: ' + file + ' index: ' + index, verbose)
-      const xmljsResult = xmljs.xml2js(data, {
-        compact: true,
-        textKey: '_',
-        attributesKey: '$',
-      })
-      endTimer('xml parsing file: ' + file + ' index: ' + index, verbose)
-      // console.dir(xmljsResult, {showHidden: true, depth: null, colors: true})
-      resolve(xmljsResult)
-      // xml2js.parseString(data, (e, r) => {
-      //   // console.dir(r, {showHidden: true, depth: null, colors: true})
-      //   resolve(r)
-      // })
-    })
-  })
-  // .then((result) => {
-  //   return result
-  // })
-}
-
-async function getNodesOfMeta(file, index, meta, verbose: boolean) {
-  return getNodesFromXmlFile(file, index, verbose).then((result) => {
-    if (result[meta]) {
-      return result[meta]
-    }
-    return {}
-  })
-}
-
 async function treatNodeUniqueKey(localType, localNode, configJson) {
   const result = []
   let nodelist = localNode
@@ -169,37 +136,6 @@ async function treatNodeUniqueKey(localType, localNode, configJson) {
   return result
 }
 
-// eslint-disable-next-line max-params
-async function getKeyedNodesOfMeta(
-  file,
-  index,
-  meta,
-  configJson,
-  verbose: boolean,
-) {
-  return getNodesOfMeta(file, index, meta, verbose).then(async (result) => {
-    startTimer('keying file: ' + file + ' index: ' + index, verbose)
-    if (result) {
-      const keyedTab = []
-      const tabPromise = []
-      for (const localType of Object.keys(result)) {
-        tabPromise.push(
-          treatNodeUniqueKey(localType, result[localType], configJson),
-        )
-      }
-      await Promise.all(tabPromise).then((data) => {
-        for (const elem of data) {
-          Object.assign(keyedTab, elem)
-        }
-      })
-      endTimer('keying file: ' + file + ' index: ' + index, verbose)
-      return keyedTab
-    }
-    endTimer('keying file: ' + file + ' index: ' + index, verbose)
-    return {}
-  })
-}
-
 export async function getKeyedFiles(
   files: string[],
   meta,
@@ -210,7 +146,59 @@ export async function getKeyedFiles(
   let output
   for (const index of files.keys()) {
     tabPromise.push(
-      getKeyedNodesOfMeta(files[index], index, meta, configJson, verbose),
+      fsp
+        .readFile(files[index], {flag: 'r', encoding: 'utf8'})
+        .then((data) => {
+          startTimer(
+            'xml parsing file: ' + files[index] + ' index: ' + index,
+            verbose,
+          )
+          const xmljsResult = xmljs.xml2js(data, {
+            compact: true,
+            textKey: '_',
+            attributesKey: '$',
+          })
+          endTimer(
+            'xml parsing file: ' + files[index] + ' index: ' + index,
+            verbose,
+          )
+          return xmljsResult
+        })
+        .then((result) => {
+          if (result[meta]) {
+            return result[meta]
+          }
+          return {}
+        })
+        .then(async (result) => {
+          startTimer(
+            'keying file: ' + files[index] + ' index: ' + index,
+            verbose,
+          )
+          if (result) {
+            const keyedTab = []
+            const tabPromise = []
+            for (const localType of Object.keys(result)) {
+              tabPromise.push(
+                treatNodeUniqueKey(
+                  localType,
+                  result[localType],
+                  configJson,
+                ).then((elem) => {
+                  Object.assign(keyedTab, elem)
+                }),
+              )
+            }
+            await Promise.all(tabPromise)
+            endTimer(
+              'keying file: ' + files[index] + ' index: ' + index,
+              verbose,
+            )
+            return keyedTab
+          }
+          endTimer('keying file: ' + files[index] + ' index: ' + index, verbose)
+          return {}
+        }),
     )
   }
   await Promise.all(tabPromise)
